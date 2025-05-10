d405fe9ba16ca3fd8f4992eeddf8b57601d5b28a
Lumie123 <jimmyminns01@gmail.com>
2025-05-10 14:02:38 +0100

    Added tokens

---
+++ src/main/java/apocRogueBE/Security/TokenPurger.java
@@ -0,0 +1,23 @@
+package apocRogueBE.Security;
+
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+import apocRogueBE.SingletonConnection.DataSourceSingleton;
+
+import java.io.BufferedWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class TokenPurger implements HttpFunction {
+    @Override
+    public void service(HttpRequest req, HttpResponse resp) throws Exception {
+        // delete all expired sessions
+        try (Connection c = DataSourceSingleton.getConnection();
+             PreparedStatement ps = c.prepareStatement(
+                     "DELETE FROM Session WHERE expires_at < NOW()")) {
+            int deleted = ps.executeUpdate();
+            resp.getWriter().write("{\"deleted\":" + deleted + "}");
+        }
+    }
+}
